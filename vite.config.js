9e4cab1dd2f44e6b9ffb0c7ae483ab91b72a7a90
ployyyyyyyyyyy <128827496+ployyyyyyyyyyy@users.noreply.github.com>
2023-03-25 00:47:10 +0700

    vite conf update

--- vite.config.js
+++ vite.config.js
@@ -8,14 +8,14 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: [
-        /^node:.*/,
-        'vue'
-      ]
-    }
-  },
+  // build: {
+  //   rollupOptions: {
+  //     external: [
+  //       /^node:.*/,
+  //       'vue'
+  //     ]
+  //   }
+  // },
   plugins: [
 		vue(),
 		vuetify({ autoImport: true }),
@@ -23,7 +23,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@vuepic': 'node_modules/@vuepic'
+      // '@vuepic': 'node_modules/@vuepic'
     }
   }
 })
