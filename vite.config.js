f7650652257228149728408829c4741d3ea9d63f
ployyyyyyyyyyy <128827496+ployyyyyyyyyyy@users.noreply.github.com>
2023-03-25 00:09:52 +0700

    succ

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,13 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: [
+        /^node:.*/,
+      ]
+    }
+  },
   plugins: [
 		vue(),
 		vuetify({ autoImport: true }),
